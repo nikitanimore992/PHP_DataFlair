bfb56b2150932a73cefc7d5d9c1f682c6c8e6ac3
nikitanimore992 <992nikitanimore@gmail.com>
2025-09-22 23:52:51 +0530

    create new operator page

---
+++ 05operators.php
@@ -0,0 +1,14 @@
+
+<html>
+<head>
+    <title>operators</title>
+</head>
+<body>
+    <center>
+        <marquee>this is php page</marquee>
+            <?php 
+            
+            ?>
+    </center>
+</body>
+</html>
