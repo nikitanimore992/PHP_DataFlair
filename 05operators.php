aa733dcac062e9d5a9265d5b9c5df82e38dde857
nikitanimore992 <992nikitanimore@gmail.com>
2025-09-23 00:23:09 +0530

    describe something

--- 05operators.php
+++ 05operators.php
@@ -7,7 +7,25 @@
     <center>
         <marquee>this is php page</marquee>
             <?php 
-            
+            // operators to 3 type me devide karte he 
+            // unary operator, binary operator, ternary operator
+            // unary operatoe - wo operator jo ek hi operand ke sath use hota he for exaple increment operator(++) and decrement operator(--) ~a not a
+            // binary operator - wo operator jo do operand ke sath use hota he for example addition(+), subtraction(-), multiplication(*), division(/), modulus(%)
+            // ternary operator - wo operator jo 3 operand ke sath use hota he for example (condition ? true : false)
+            // 1. arithmetic operator - +, -, *, /, %
+            // 2. assignment operator - =, +=, -=, *=, /=, %=
+            // 3. comparison operator - ==, !=, >, <, >=, <=
+            // 4. logical operator - &&, ||, !
+            // 5. string operator - ., .=
+            // 6. array operator - +, ==, !=, ===, !==
+            // 7. conditional operator - ? :
+            // 8. bitwise operator - &, |, ^, ~, <<, >>
+            // 11. increment/decrement operator - ++, --
+            // 12. type operator - instanceof
+
+
+
+
             ?>
     </center>
 </body>
