f38597200ace1bac4a310196c147de42e7f9c052
nikitanimore992 <992nikitanimore@gmail.com>
2025-09-24 00:42:15 +0530

    conditional assignmet operator

--- 07operatores.php
+++ 07operatores.php
@@ -24,6 +24,16 @@
                 $d = 10;
                 $d.= $c; // $d = $d . $c
                 echo $d; // 1010 yaha kya hua is operater ne do value ko jod kar ek nayi value banayi or ye sab strig value ke form me hota he addtion nahi hota he .
+
+
+                //Constional assingnment operator - (condition ? true : false)
+                // for exam.
+                $age = 20;
+                echo ($age>18) ? "you are eligible for vote" : "you are not eligible for vote"; // you are eligible for vote
+                echo "<br>";
+                $str = 20? "this is true" : "this is false";
+                echo $str; // this is true
+                echo "<br>";
                 ?>
     </center>
 </body>
