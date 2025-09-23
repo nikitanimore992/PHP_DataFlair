b7030a5fb15643abb463b10548c33012893bbe56
nikitanimore992 <992nikitanimore@gmail.com>
2025-09-24 00:43:46 +0530

    create a new page'

---
+++ 08function.php
@@ -0,0 +1,15 @@
+
+
+<html>
+<head>
+    <title>this is php code page</title>
+</head>
+<body>
+    <center>
+        <marquee>this is php function page</marquee>
+            <?php 
+            
+            ?>
+    </center>
+</body>
+</html>
