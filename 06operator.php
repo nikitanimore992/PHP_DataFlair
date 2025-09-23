bf0e2101adcceb94eb6ac01a3475fc11d1d81b39
nikitanimore992 <992nikitanimore@gmail.com>
2025-09-24 00:23:11 +0530

    operator apage

--- 06operator.php
+++ 06operator.php
@@ -7,8 +7,43 @@
     <center>
         <marquee>this is php page</marquee>
             <?php 
-            $a =20;
-            
+            $a =70;
+            $b =7;
+            if(++$a>50 && ++$b<5){
+                echo "hello friends";
+            }else{
+                echo"bye friends";
+            }
+                echo "<br>";
+            //--------------------------------------------------------------------------------------
+              if(++$a>50 && ++$b<10){
+                echo "hello friends";
+            }else{
+                echo"bye friends";
+            }
+             echo "<br>";
+            //--------------------------------------------------------------------------------------
+              if(++$a>100 && ++$b<10){ // isme first condition false ho jane per age wo condion check hi nahi karege kyu end operator me dono hi condtion true honi chahiye true tab hoga jab dono hi condition true honge
+                echo "hello friends";
+            }else{
+                echo"bye friends";
+            }
+             echo "<br>";
+             //--------------------------------------------------------------------------------------
+              if(++$a>50 || ++$b<5){ // isme first condition true ho jane per age wo condion check hi nahi karege kyu end operator me dono hi condtion false honi chahiye tab hi wo false return karega
+                echo "hello friends";
+            }else{
+                echo"bye friends";
+            }
+             echo "<br>";
+
+             //--------------------------------------------------------------------------------------
+              if(!(++$a>50 && ++$b<10)){//! operatore condition ka thik ulta kar deta he like if result is true then it returns false and if result is false then it returns true
+                echo "hello friends";
+            }else{
+                echo"bye friends";
+            }
+             echo "<br>";
             ?>
     </center>
 </body>
