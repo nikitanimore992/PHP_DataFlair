f61f73549f6e0254df6222712a19a54b38a654e5
nikitanimore992 <992nikitanimore@gmail.com>
2025-09-24 13:20:27 +0530

    change

--- 08function.php
+++ 08function.php
@@ -8,7 +8,20 @@
     <center>
         <marquee>this is php function page</marquee>
             <?php 
+            //  a fucntion is a program which is basically use to reduce the size of main program and to make it more readable and to avoid the repitition of code
+            // as you can say small modul of the program
             
+            function display(){ // defining a function
+                echo "hello friends";
+            }
+            display(); // calling a function
+            echo " this is fuction";
+            echo "<br>"
+
+            function sum($a, $b){
+                $c = $a + $b;
+                return $c;
+            }
             ?>
     </center>
 </body>
