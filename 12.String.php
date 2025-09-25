80e119113ed14c575235e4b2c476e60b5981a76b
nikitanimore992 <992nikitanimore@gmail.com>
2025-09-26 00:43:22 +0530

    str_word_count() function

--- 12.String.php
+++ 12.String.php
@@ -9,10 +9,19 @@
             <?php 
             // string functions
 
+            // strlen() function
             $str = "Hello World";
             echo $str."<br>";
-            echo "string length is : ".strlen($str); 
+            echo "string length is : ".strlen($str)."<br>"; 
             // string ki length caluculate karne ke liye php me ham log strlen() function ka use karte hai
+
+            // str_word_count() function
+            $mystr = "hello this is nikita Nimore";
+            $n = str_word_count($mystr);
+            echo $mystr."<br>";
+            echo "total words is: ".$n."<br>";
+            // string me kitne word hai ye count karne ke liye php me ham log str_word_count() function ka use karte hai
+           
             ?>
     </center>
 </body>
