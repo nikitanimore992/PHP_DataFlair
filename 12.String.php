ae3707c121cb2b0a6ab9d91f740508c342a842a6
nikitanimore992 <992nikitanimore@gmail.com>
2025-09-26 01:24:19 +0530

    chunk_split() function

--- 12.String.php
+++ 12.String.php
@@ -22,6 +22,18 @@
             echo "total words is: ".$n."<br>";
             // string me kitne word hai ye count karne ke liye php me ham log str_word_count() function ka use karte hai
            
+            // chunk_split() function
+            $mystring = "Hello this is nikita nimore";
+            $newstr = chunk_split($mystring,3," Ankit ");
+            echo "my new string is ====> ".$newstr."<br>";
+            // man lijiye apko apne string me kuch new add krna he and another new string banana he then you can go for chunk_split() function 
+            // and this is immutable String  means not allow to chane themself for example jese hamne prev. me kiya the agar ham without new string create kiye bina hi direct actual string me change karna chahe to wo change nahi karega just beacause string is immutable string khud me change nahi karega until unless ap agar new string create karte hai to wo changes apko us string me reflect honge.
+            $mystringg = "Hello this is nikita nimore";
+            chunk_split($mystringg,3,".");
+            echo $mystringg."<br>"; // yaha par kuch bhi change nahi hoga beacause string is immutable.
+
+      
+
             ?>
     </center>
 </body>
