c3667566fdb79112a3c67c85b5985d8564a72d8b
nikitanimore992 <992nikitanimore@gmail.com>
2025-09-25 17:53:08 +0530

    factorial function

---
+++ 10FactorialbyFunction.php
@@ -0,0 +1,14 @@
+
+<html>
+<head>
+    <title>this is php code page</title>
+</head>
+<body>
+    <center>
+        <marquee>this is factorial function page</marquee>
+            <?php 
+
+               ?>
+    </center>
+</body>
+</html>
