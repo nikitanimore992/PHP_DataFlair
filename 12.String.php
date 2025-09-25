019123d3fd8ea3707ea372cd92f41a4ee261f5aa
nikitanimore992 <992nikitanimore@gmail.com>
2025-09-26 00:36:35 +0530

    strlen() in php

--- 12.String.php
+++ 12.String.php
@@ -7,7 +7,12 @@
     <center>
         <marquee>this is string page</marquee>
             <?php 
-           
+            // string functions
+
+            $str = "Hello World";
+            echo $str."<br>";
+            echo "string length is : ".strlen($str); 
+            // string ki length caluculate karne ke liye php me ham log strlen() function ka use karte hai
             ?>
     </center>
 </body>
