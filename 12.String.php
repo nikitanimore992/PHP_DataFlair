8ca7f20420983b5893a66ae114bdffa9236d6ec0
nikitanimore992 <992nikitanimore@gmail.com>
2025-09-26 00:31:05 +0530

    create a new php file

---
+++ 12.String.php
@@ -0,0 +1,14 @@
+
+<html>
+<head>
+    <title>this is php code page</title>
+</head>
+<body>
+    <center>
+        <marquee>this is string page</marquee>
+            <?php 
+           
+            ?>
+    </center>
+</body>
+</html>
