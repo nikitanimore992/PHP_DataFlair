b2cd5955a1d10fa60c65a1f5ca9d5c983f12cbfb
nikitanimore992 <992nikitanimore@gmail.com>
2025-09-25 21:50:00 +0530

    factorial , swaping , sum,

--- 10FactorialbyFunction.php
+++ 10FactorialbyFunction.php
@@ -31,6 +31,40 @@
                 echo "<br>";
 
                 // dusra hota he  ki jab function ko ham call karte he with the the call - call by value function
+
+                // call by value
+                function show($a,$b){
+                    $a = $a + 10;
+                    $b = $b + 20;
+                }
+                $a = 50;
+                $b = 100;
+                show($a,$b); // call by value
+                echo $a." ".$b;
+                    echo "<br>";
+                // call by reference
+                function shows(&$c,&$d){ // & ye sign lagane se wo call by reference ho jata he
+                    $c = $c + 10;
+                    $d = $d + 20;
+                }
+                $c = 50;
+                $d = 100;
+                shows($c,$d); // call by value
+                echo $c." ".$d;
+                echo "<br>";
+
+                    // another example of call by reference
+                    function swap(&$a,&$b){
+                        $c = $a;
+                        $a = $b;
+                        $b = $c;
+                    }
+                    $a=60;
+                    $b=90; 
+                    echo "<br>Before swaping ".$a." ".$b;
+                    swap($a,$b);
+                    echo "<br>after swaping ".$a." ".$b;
+                    echo "<br>";
                ?>
     </center>
 </body>
