8fcb1471a8e148f885e36b560bf615edb1b773a6
nikitanimore992 <992nikitanimore@gmail.com>
2025-09-26 23:34:38 +0530

    change

--- 14Array.php
+++ 14Array.php
@@ -15,6 +15,11 @@
    $newAray = array_intersect($array1,$array2);
    print_r($newAray);
    // array_intersect() ek predifine function array ka jiska use karke do arrays ke bich me jo bhi element common he use print karta he a
+    echo"<br>";
+   $newAr = array_diff($array1,$array2);
+   print_r($newAr);
+   echo "<br>";
+//    yaha kya ho raha 
     ?>
     
 </body>
