dfc4cdc7dfecdef2d81191a19e677be27afe3370
nikitanimore992 <992nikitanimore@gmail.com>
2025-09-27 17:45:23 +0530

    array_slice

--- 14Array.php
+++ 14Array.php
@@ -16,10 +16,17 @@
    print_r($newAray);
    // array_intersect() ek predifine function array ka jiska use karke do arrays ke bich me jo bhi element common he use print karta he a
     echo"<br>";
+
    $newAr = array_diff($array1,$array2);
    print_r($newAr);
    echo "<br>";
-//    yaha kya ho raha 
+    //    yaha per ham don array ko complare kar rahe he per out me hame only sat == hi mila 
+    // yaha pr array1 to array to se compare kar rahe jism ejo bhi element array one array2 me nahi he bs use hi show karega matlb kuch esa jo only array 2 me he but array1 me nahi nahi show nahi hoga
+
+    $newa = array_slice($array1,1,3,true);
+    print_r($newa);
+    // iska use bade array se koi particual array nikalna he to uske liye use karte he 
+
     ?>
     
 </body>
