bb959feef96b5ef41c6c6741ec6050f82f0c2656
nikitanimore992 <992nikitanimore@gmail.com>
2025-10-09 15:09:43 +0530

    create a userlogin page

---
+++ 15UserLogin.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <title>Document</title>
+</head>
+<body>
+    methods - get and post using this method for get data or psot data from client side side to server side. 
+    heder(Location:page name); use  for page redirection if your want to move another page 
+    isset() iska use jab ham current file me is php code likhte se to chahte he ki hamare kisi specic word ke baad wo code chale
+    like - i want username and pass - want that data after user submit that data  
+    isset() you can write like this 
+    if(isset($_POST['actionname'])){
+        code...... that your want to perform after action;
+    }
+</body>
+</html>
