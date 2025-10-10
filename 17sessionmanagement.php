781565fba24965c62af3d6cf68d3ff981d4b3846
nikitanimore992 <992nikitanimore@gmail.com>
2025-10-10 15:59:47 +0530

    describe session management

--- 17sessionmanagement.php
+++ 17sessionmanagement.php
@@ -4,9 +4,16 @@
 
 <body>
 <center>
-   <?php
+      <!-- this is very important topic for project and interview perpose
 
-?> 
+      jab bhi  ham ek page se dusre page se data lekr jate he wo easialy move ho jata he lekin thir page pr le jane me hame problem face karni padti he 
+      or agr hame apne data ki sates ko continusly manage karna he 
+      
+      apne server page pr to ham session manaegment ka use kar sakte he  iska use karte ham apne data ki states ko manage kar sakte he ye problem ham isiliye face karte he kyuki hamara data jin server ka use karke data share karta he wo statless hote he  
+      means inki jo state he wo only one page tak manage hoti he lekin second page manage karna difficult ho jata he or agar hame manage karna he to ham session management ka use kr sakte hai -->
+   <?php
+      
+    ?> 
 </center>
 </body>
 </html>
