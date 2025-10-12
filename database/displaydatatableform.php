6e62439aabcee9c490412ae02745acc84d4bb77c
nikitanimore992 <992nikitanimore@gmail.com>
2025-10-12 23:55:44 +0530

    done

--- database/displaydatatableform.php
+++ database/displaydatatableform.php
@@ -11,15 +11,23 @@ $n=mysqli_num_rows($record);
 echo "total record<br>";
 if($n>0){
     // echo $n;
-
-    while($row=mysqli_fetch_row($record)){ // record object se data fetch karne ke liye is function use karte hai 
-        echo $row[0]." ".$row[1]." ".$row[2]." ".$row[3]." ";
+    echo "<table border=1>";
+    echo "<tr><th>Emp ID</th><th>Emp Name</th><th>Emp salary</th><th>department</th></tr>";
+    while($row=mysqli_fetch_assoc($record)){ // record object se data fetch karne ke liye is function use karte hai 
+        echo "<tr>";
+        echo "<td>".$row['empid']."</td>";
+        echo "<td>".$row['empname']."</td>";
+        echo "<td>".$row['salary']."</td>";
+        echo "<td>".$row['department']."</td>";
+        echo "</tr>";
         echo "<br>";
     }
+    echo "</table>";
 }
 else{
     echo "Record not found";
 }
+$mycon->close();
 
 ?>
 </body>
