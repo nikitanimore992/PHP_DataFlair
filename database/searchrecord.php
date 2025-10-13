e9938524719c91a59ab59c67fb8dae0a0c972c59
nikitanimore992 <992nikitanimore@gmail.com>
2025-10-13 23:38:37 +0530

    update serarch page

---
+++ database/searchrecord.php
@@ -0,0 +1,22 @@
+<html>
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>search page </title>
+    </head>
+    <body>
+         <center>
+            <form action="Studentdata.php" method="post"></form>
+            <table>
+                <tr>
+                    <th>Student Id</th>
+                    <td><input type="text" name="txtid"></td>
+                 </tr>
+                <tr>
+                    <td></td>
+                    <td><input type="submit" value="Search" name="btnSbumit"></td>
+                </tr>
+            </table>
+         </center>
+    </body>
+    </html>
