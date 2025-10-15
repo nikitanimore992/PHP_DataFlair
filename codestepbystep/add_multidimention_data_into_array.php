e21e030908af797c84f3c47c9fe34692d38586aa
nikitanimore992 <992nikitanimore@gmail.com>
2025-10-15 23:57:34 +0530

    add new data

--- codestepbystep/add_multidimention_data_into_array.php
+++ codestepbystep/add_multidimention_data_into_array.php
@@ -19,16 +19,22 @@
             <td>nikitanimore@gmail.com</td>
         </tr>
         <tr>
-            <td>1</td>
+            <td>2</td>
             <td>Neeraj</td>
             <td>harda</td>
             <td>NeerajRathore@gmail.com</td>
         </tr>
         <tr>
-            <td>1</td>
+            <td>3</td>
             <td>Neha</td>
             <td>Indore</td>
             <td>nehanimore@gmail.com</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>Yogita</td>
+            <td>Indrapuri</td>
+            <td>Yogitanimore@gmail.com</td>
         </tr>
     </table>
   </center>  
