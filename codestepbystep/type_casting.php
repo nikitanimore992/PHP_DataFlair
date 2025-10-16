a44d03930f3a5a0789120bb383c8a7f9d776b00d
nikitanimore992 <992nikitanimore@gmail.com>
2025-10-16 16:21:23 +0530

    type casting

---
+++ codestepbystep/type_casting.php
@@ -0,0 +1,35 @@
+
+<?php 
+ 
+
+  $a = 5;
+  var_dump($a);
+  $a = (String) $a;
+  var_dump($a);
+  echo "<br>";
+
+  $a = 5;
+  var_dump($a);
+  $a = (bool) $a;
+  var_dump($a);
+  echo "<br>";
+
+  $a = true;
+  var_dump($a);
+  $a = (int) $a;
+  var_dump($a);
+
+  echo "<br>";
+  $a = "300";
+  var_dump($a);
+  $a = (int) $a;
+  var_dump($a);
+
+  echo "<br>";
+  $a = 5;
+  var_dump($a);
+  $a = (bool) $a;
+  var_dump($a);
+
+
+?>
