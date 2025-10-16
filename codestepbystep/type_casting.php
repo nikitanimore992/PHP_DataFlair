fa919b67a1c08905c1519c0ddaf42012ad4bbb0c
nikitanimore992 <992nikitanimore@gmail.com>
2025-10-16 16:23:08 +0530

    array convertion

--- codestepbystep/type_casting.php
+++ codestepbystep/type_casting.php
@@ -26,10 +26,15 @@
   var_dump($a);
 
   echo "<br>";
-  $a = 5;
+  $a = 0;
   var_dump($a);
   $a = (bool) $a;
   var_dump($a);
 
+  echo "<br>";
+  $a = 5;
+  var_dump($a);
+  $a = (array) $a;
+  var_dump($a);
 
 ?>
