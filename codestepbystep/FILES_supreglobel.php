2a5e7a9d93481b8bb50d19a453f1dbfbb2937e8b
nikitanimore992 <992nikitanimore@gmail.com>
2025-10-28 23:56:21 +0530

    image upload

--- codestepbystep/FILES_supreglobel.php
+++ codestepbystep/FILES_supreglobel.php
@@ -4,11 +4,33 @@
    <title>php</title>
 </head>
 <body>
-   <form action="./FILES_superglobal.php" method="post" enctype="multipart/form-data">
+   <!-- action="./FILES_superglobal.php" -->
+   <form  action="" method="post" enctype="multipart/form-data">
     <input type="file" name="fileupload" id="fileupload">
     <br><br><br>
-    <button>upload</button>
+    <button type="submit" name="uplbtn">upload</button>
    </form> 
   
 </body>
 </html>
+
+<?php 
+   if(isset($_POST['uplbtn'])){
+
+      if($_FILES['fileupload']){
+      $path = $_FILES['fileupload']['name'];
+      $upload_path = "./uploads/".$path;
+
+      //  move_uploaded_file($_FILES['fileupload']['tmp_name'],$upload_path)|| die ("Filed to upload ");
+      if(move_uploaded_file($_FILES['fileupload']['tmp_name'],$upload_path)){
+         echo "file upoad success";
+      }else{
+         die ("faild to upload file");
+      }
+
+   }else{
+      echo "no file Found";
+   }
+
+   }
+?>
