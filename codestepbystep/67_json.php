7b640cab257b5b4d051add73bf587e1fad4c4089
nikitanimore992 <992nikitanimore@gmail.com>
2025-11-03 22:51:14 +0530

    json

---
+++ codestepbystep/67_json.php
@@ -0,0 +1,10 @@
+
+Description----------------
+
+    - json - stand for javascript object notation. 
+
+    - it is a text-based data exchange formate for data structuring data.
+        - text-based - means all type of data is working on it.
+        - data exchange - json kihelp se ek server se dusre server me or ek programming language se dusri programming ke andar data ko exchange kar sakte he data structuring ke liye (like - search, update, delete, add)
+
+    - it is mainly used to exchange data between a server and web application. 
