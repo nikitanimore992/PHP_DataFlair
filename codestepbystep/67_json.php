3f0a3ffdd9a364c3ae96cd04aecd21ebfb48ff95
nikitanimore992 <992nikitanimore@gmail.com>
2025-11-03 23:16:00 +0530

    create a new file

--- codestepbystep/67_json.php
+++ codestepbystep/67_json.php
@@ -8,3 +8,7 @@ Description----------------
         - data exchange - json kihelp se ek server se dusre server me or ek programming language se dusri programming ke andar data ko exchange kar sakte he data structuring ke liye (like - search, update, delete, add)
 
     - it is mainly used to exchange data between a server and web application. 
+
+why we call only json- javascript object notation only - javascript  ke object same like json (java
+script notation) and only js inherit json thats why we  say json only
+
