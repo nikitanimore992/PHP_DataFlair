92074750e1dd869a278c77b5b55e3f208822b149
nikitanimore992 <992nikitanimore@gmail.com>
2025-11-05 01:25:14 +0530

    json to array decode and array to json encode

--- codestepbystep/67_json_.php
+++ codestepbystep/67_json_.php
@@ -1,4 +1,18 @@
 <?php
+// convert array to json 
+$user = ["Name"=>"Nikita","Age"=>24,"Email"=>"nikitanimore@gmail.com"];
+print_r ($user);
+$userJson= json_encode($user);
+echo $userJson;
 
+// conver json data to array
+$data='{"Name":"Neha","Age":23,"Email":"nehanimore@gmail.com"}';
+echo $data;
+echo "<br>";
+$dataArray = json_decode($data);
+print_r ($dataArray);
+echo "<br>";
+$datasArray = json_decode($data,true);
+print_r ($datasArray);
 
 ?>
