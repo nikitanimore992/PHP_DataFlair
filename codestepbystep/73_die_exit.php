0d8d625390ea66bbffa596f9fcbba6283876b191
nikitanimore992 <992nikitanimore@gmail.com>
2025-11-05 15:57:07 +0530

    die and exit

---
+++ codestepbystep/73_die_exit.php
@@ -0,0 +1,23 @@
+<?php
+// jab hame code execution rokni hoti he  die and exit ,  and debugging ke liye bhi ham iska use karte he 
+// ek function se output get karne ke liye return ka use karte he and execution rokne ke liye bhi karte he but limited
+// exit c language se liya gaya he jabki die  ko perl programming language se liya gaya he  
+
+
+$a = 10;
+$b= 20;
+echo $a-$b;
+echo "<br>";
+
+echo $a+$b;
+echo "<br>";
+//die(); exit()// if i want ki mera code bs yaha tak excute to usse age execute ho hi nahi
+echo $a*$b;
+echo "<br>";
+
+echo $a/$b;
+echo "<br>";
+
+
+
+?>
